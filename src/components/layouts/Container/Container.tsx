0eab43d7a10f1e82df35dfccffaa2ab0f673e871
ManishR426513 <manish@delimp.com>
2025-03-12 17:50:40 +0530

    setup

---
+++ src/components/layouts/Container/Container.tsx
@@ -0,0 +1,35 @@
+import React, { forwardRef, ReactNode } from 'react';
+import classNames from 'classnames';
+
+type TContainerBreakpoint =
+	| 'container'
+	| 'sm:container'
+	| 'md:container'
+	| 'lg:container'
+	| 'xl:container'
+	| '2xl:container'
+	| null;
+
+interface IContainerProps {
+	children: ReactNode;
+	className?: string;
+	breakpoint?: TContainerBreakpoint;
+}
+
+const Container = forwardRef<HTMLDivElement, IContainerProps>((props, ref) => {
+	// eslint-disable-next-line react/prop-types
+	const { children, className, breakpoint = 'container', ...rest } = props;
+
+	return (
+		<div
+			ref={ref}
+			data-component-name='Container'
+			className={classNames('mx-auto p-4', breakpoint, className)}
+			{...rest}>
+			{children}
+		</div>
+	);
+});
+Container.displayName = 'Container';
+
+export default Container;
