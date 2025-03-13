d5c81dbf308132c352ac3a683eb6c9f49cb735f7
ManishR426513 <manish@delimp.com>
2025-03-13 11:08:06 +0530

    Add initial layout components and routing setup

---
+++ src/Layout/PrivateLayout.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+import TopBar from "@/common/Topbar";
+import { AppSidebar } from "@/components/app-sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+
+interface PrivateLayoutProps {
+  children: ReactNode;
+}
+
+export default function PrivateLayout({ children }: PrivateLayoutProps) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <TopBar />
+        {children}
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
--- src/routing/index.tsx
+++ src/routing/index.tsx
@@ -1,13 +1,23 @@
 import { LoginForm } from "@/common/Login";
+import Home from "@/pages/Home/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router";
-
+import PrivateRoute from "./PrivateRoute";
 
 export default function Routing() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LoginForm />} />
-        
+        <Route path="/login" element={<LoginForm />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute>
+              
+              <Home />
+            </PrivateRoute>
+          }
+        />
+
         {/* <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} /> */}
       </Routes>
