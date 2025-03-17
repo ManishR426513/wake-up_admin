e7a378f1426f18308d7942bb60bf392af4112ecd
ManishR426513 <manish@delimp.com>
2025-03-17 17:56:30 +0530

    config url changes

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -1,10 +1,9 @@
 import axios, { AxiosInstance } from "axios";
 
-const appurl="http://localhost:8080"
 
 export const authAxios = (token?: string): AxiosInstance => {
     return axios.create({
-        baseURL: `${appurl}/api/admin`,
+        baseURL: `${process.env.REACT_APP_BASEURL}/api/admin`,
         headers: {
             'Authorization': `${token ? `${token}` : ''}`,
         },
